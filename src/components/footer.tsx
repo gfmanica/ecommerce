48b5611c16ea183b7aeac75e708e38709709be03
gfmanica <gabrielfelipemanica@gmail.com>
2023-11-16 22:56:02 -0300

    ajustado footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="mt-auto bg-gradient-to-r from-sky-100 to-indigo-100 h-12 px-12 flex items-center justify-center text-center">
+    <div className="mt-auto bg-gradient-to-r from-sky-100 to-indigo-100 h-12 px-12 py-8 flex items-center justify-center text-center">
       Gabriel Manica© {year} - Todos os direitos reservados
     </div>
   );
